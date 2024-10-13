12782e4174960e79c42bf97842cf6dffba27efe2
muriisantuss <muriisantuss@gmail.com>
2024-10-13 04:53:43 -0300

    :sparkles: projeto02: task 04 - finishProject

--- project-two/js/js.js
+++ project-two/js/js.js
@@ -1,17 +1,35 @@
+let userHistory = [];
+
 function search() {
   var user = document.getElementById("inputUserName").value;
   let url = `https://api.github.com/users/${user}`;
 
   $.getJSON(url, (u) => {
-    clearError()
+    clearError();
     setUserData(u);
+    if (isNew(u)) {
+      save(u);
+      getHistory(u);
+    }
   }).fail(() => {
-    setUserData();
+    clearUserData();
     showError("ERROR 404 - USUARIO NÃO ENCONTRADO");
   });
 }
 
+
+function save(u) {
+  userHistory.push(u);
+}
+function isNew(u) {
+  return userHistory.filter((user) => user.login === u.login).length == 0;
+}
+function getHistory(u) {
+  document.getElementById("history").innerHTML += `
+    <div class="col"><img src=${u.avatar_url} alt="" width="110" height="100" class="rounded-3 shadow"></div>`;
+}
 function showError(msg) {
+  console.log("Erro:", msg); 
   document.getElementById("errorMessage").innerHTML = msg;
 }
 function clearError() {
@@ -25,3 +43,12 @@ function setUserData(u) {
     ? `<img src=${u.avatar_url} alt="" width="200" height="200" class="rounded-3 shadow">`
     : "";
 }
+
+function clearUserData() {
+  document.getElementById("name").innerHTML = "";
+  document.getElementById("url").innerHTML =  "";
+  document.getElementById("company").innerHTML = "";
+  document.getElementById("avatar_url").innerHTML = ""
+}
+
+
