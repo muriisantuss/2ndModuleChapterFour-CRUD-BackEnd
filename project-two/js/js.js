f67895a269fb5c21bfd3f4f02abf981b09ad7314
muriisantuss <muriisantuss@gmail.com>
2024-10-13 03:50:38 -0300

    :sparkles: projeto02: task 02 - search

--- project-two/js/js.js
+++ project-two/js/js.js
@@ -1,1 +1,14 @@
-$get.json('api.github.com/users')
+function search() {
+  
+  var user = document.getElementById("inputUserName").value;
+  let url = `https://api.github.com/users/${user}`;
+
+  $.getJSON(url, (u) => {
+    document.getElementById("name").innerHTML = u.name;
+    document.getElementById("url").innerHTML = u.html_url;
+    document.getElementById("company").innerHTML = u.company;
+    document.getElementById("avatar_url").innerHTML = `
+      <img src=${u.avatar_url} alt="" width="200" height="200" class="rounded-3 shadow">
+    `;
+  });
+}
